e303c191fa47c709b8c5e794664ec9f525932ee0
Friski Pradana <49704585+friskipradana@users.noreply.github.com>
2020-01-04 12:38:20 +0700

    Update computervision.php

--- computervision.php
+++ computervision.php
@@ -58,7 +58,7 @@ if (isset($_POST['submit'])) {
             // *** Update or verify the following values. ***
             // **********************************************
             // Replace <Subscription Key> with your valid subscription key.
-            var subscriptionKey = "5244d0b5afb846009d2d0c8f14313367";
+            var subscriptionKey = "11a80a1fd7f940658116e077ee63406f";
             // You must use the same Azure region in your REST API method as you used to
             // get your subscription keys. For example, if you got your subscription keys
             // from the West US region, replace "westcentralus" in the URL
