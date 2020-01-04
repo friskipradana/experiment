41d8db669c82ae1dece2ca17162095802efea3a0
Friski Pradana <49704585+friskipradana@users.noreply.github.com>
2020-01-04 11:48:54 +0700

    Update computervisions.php

--- computervisions.php
+++ computervisions.php
@@ -7,6 +7,50 @@ if (isset($_POST['submit'])) {
 	}
 } else {
 	header("Location: analyze.php");
+// NOTE: Be sure to uncomment the following line in your php.ini file.
+// ;extension=php_openssl.dll
+// You might need to set the full path, for example:
+// extension="C:\Program Files\Php\ext\php_openssl.dll"
+
+$subscription_key = "11a80a1fd7f940658116e077ee63406f";
+$endpoint = "https://visioncomps.cognitiveservices.azure.com/";
+
+$path = '/text/analytics/v2.1/languages';
+
+function DetectLanguage ($host, $path, $key, $data) {
+
+    $headers = "Content-type: text/json\r\n" .
+        "Ocp-Apim-Subscription-Key: $key\r\n";
+
+    $data = json_encode ($data);
+
+    // NOTE: Use the key 'http' even if you are making an HTTPS request. See:
+    // https://php.net/manual/en/function.stream-context-create.php
+    $options = array (
+        'http' => array (
+            'header' => $headers,
+            'method' => 'POST',
+            'content' => $data
+        )
+    );
+    $context  = stream_context_create ($options);
+    $result = file_get_contents ($host . $path, false, $context);
+    return $result;
+}
+
+$data = array (
+    'documents' => array (
+        array ( 'id' => '1', 'text' => 'This is a document written in English.' ),
+        array ( 'id' => '2', 'text' => 'Este es un document escrito en Español.' ),
+        array ( 'id' => '3', 'text' => '这是一个用中文写的文件')
+    )
+);
+
+print "Please wait a moment for the results to appear.";
+
+$result = DetectLanguage ($endpoint, $path, $subscription_key, $data);
+
+echo json_encode (json_decode ($result), JSON_PRETTY_PRINT);
 }
 ?>
 <!DOCTYPE html>
