3fbc97917db27c7c9bf255858da7b5efceddd7d0
Friski Pradana <49704585+friskipradana@users.noreply.github.com>
2020-01-04 15:56:05 +0700

    Update computervision.php

--- computervision.php
+++ computervision.php
@@ -26,20 +26,6 @@ if (isset($_POST['submit'])) {
 
 </head>
 <body>
- <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
-			<ul class="navbar-nav mr-auto">
-			<li class="nav-item">
-				<a class="nav-link" href="https://experime.azurewebsites.net/">Home</a>
-			</li>
-			<li class="nav-item active">
-				<a class="nav-link" href="https://experime.azurewebsites.net/analyze.php">Analisis Kendaraan<span class="sr-only">(current)</span></a>
-			</li>
-		</div>
-		</nav>
 <script type="text/javascript">
     function processImage() {
         
