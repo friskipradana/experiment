2a6d3dad699416f58e95d6e6b0318a55bd8aa64e
Friski Pradana <49704585+friskipradana@users.noreply.github.com>
2020-01-04 16:29:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item ">
+      <li class="nav-item active">
         <a class="nav-link" href="https://experime.azurewebsites.net/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="https://experime.azurewebsites.net/analyze.php">Add Image</a>
       </li>
-  <li class="nav-item active">
+  <li class="nav-item">
         <a class="nav-link" href="https://experime.azurewebsites.net/computervision.php">Analyze image</a>
       </li>
     </ul>
