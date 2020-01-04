f2a7f84ee8a66229e9fe4c933ad57a1a3564a04e
Friski Pradana <49704585+friskipradana@users.noreply.github.com>
2020-01-04 12:44:23 +0700

    Update computervision.php

--- computervision.php
+++ computervision.php
@@ -108,7 +108,25 @@ if (isset($_POST['submit'])) {
         });
     </script>
 <br>
+			
+<input type="text" name="inputImage" id="inputImage"
+    value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
+<button onclick="processImage()">Analyze image</button>
+<br><br>
 <div id="wrapper" style="width:1020px; display:table;">
+    <div id="jsonOutput" style="width:600px; display:table-cell;">
+        Response:
+        <br><br>
+        <textarea id="responseTextArea" class="UIInput"
+                  style="width:580px; height:400px;"></textarea>
+    </div>
+    <div id="imageDiv" style="width:420px; display:table-cell;">
+        Source image:
+        <br><br>
+        <img id="sourceImage" width="400" />
+    </div>
+</div>
+<!-- <div id="wrapper" style="width:1020px; display:table;">
 	<div id="jsonOutput" style="width:600px; display:table-cell;">
 		<b>Response:</b>
 		<br><br>
@@ -121,7 +139,7 @@ if (isset($_POST['submit'])) {
 		<img id="sourceImage" width="400" />
 		<br>
 		<h3 id="description">Loading description. . .</h3>
-	</div>
+	</div> -->
 </div>
 </body>
 </html>
