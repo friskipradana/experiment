986a467dd5a2e2d7f21aaf3208b4d519445c0723
Friski Pradana <49704585+friskipradana@users.noreply.github.com>
2020-01-04 12:24:19 +0700

    Update computervision.php

--- computervision.php
+++ computervision.php
@@ -1,56 +1,4 @@
 <?php
-// Replace <Subscription Key> with a valid subscription key.
-$ocpApimSubscriptionKey = '11a80a1fd7f940658116e077ee63406f';
-
-// You must use the same location in your REST call as you used to obtain
-// your subscription keys. For example, if you obtained your subscription keys
-// from westus, replace "westcentralus" in the URL below with "westus".
-$uriBase = 'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/';
-
-$imageUrl =
-    'https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze';
-
-require_once 'HTTP/Request2.php';
-
-$request = new Http_Request2($uriBase . 'generateThumbnail');
-$url = $request->getUrl();
-
-$headers = array(
-    // Request headers
-    'Content-Type' => 'application/json',
-    'Ocp-Apim-Subscription-Key' => $ocpApimSubscriptionKey
-);
-$request->setHeader($headers);
-
-$parameters = array(
-    // Request parameters
-    'width' => '100',  // Width of the thumbnail.
-    'height' => '100', // Height of the thumbnail.
-    'smartCropping' => 'true',
-);
-$url->setQueryVariables($parameters);
-
-$request->setMethod(HTTP_Request2::METHOD_POST);
-
-// Request body parameters
-$body = json_encode(array('url' => $imageUrl));
-
-// Request body
-$request->setBody($body);
-
-try
-{
-    $response = $request->send();
-    echo "<pre>" .
-        json_encode(json_decode($response->getBody()), JSON_PRETTY_PRINT) . "</pre>";
-}
-catch (HttpException $ex)
-{
-    echo "<pre>" . $ex . "</pre>";
-}
-
-
-?>
 
 
 if (isset($_POST['submit'])) {
@@ -62,7 +10,7 @@ if (isset($_POST['submit'])) {
 } else {
 	header("Location: analyze.php");
 }
-?>
+
 
 <!DOCTYPE html>
     <html>
@@ -268,3 +216,4 @@ Image to analyze:
 </div>
 </body>
 </html>
+			?>
