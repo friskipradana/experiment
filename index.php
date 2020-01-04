ef724c8974193e27a5d984f11b1ca53ad832a42b
Friski Pradana <49704585+friskipradana@users.noreply.github.com>
2020-01-04 15:59:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
  </head>
  <body>
-
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
