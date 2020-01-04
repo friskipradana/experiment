047576a8f78a0fbf307e50f1e5016f9cd0c5091f
Friski Pradana <49704585+friskipradana@users.noreply.github.com>
2020-01-04 16:27:38 +0700

    Update analyze.php

--- analyze.php
+++ analyze.php
@@ -46,14 +46,17 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
   </head>
 <body>
 	
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item ">
         <a class="nav-link" href="https://experime.azurewebsites.net/">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="https://experime.azurewebsites.net/analyze.php">Analize Image</a>
+      <li class="nav-item">
+        <a class="nav-link" href="https://experime.azurewebsites.net/analyze.php">Add Image</a>
+      </li>
+  <li class="nav-item active">
+        <a class="nav-link" href="https://experime.azurewebsites.net/computervision.php">Analyze image</a>
       </li>
     </ul>
   </div>
