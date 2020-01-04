edc486587df58937b38cc92e9a20a8d293bd2f8e
Friski Pradana <49704585+friskipradana@users.noreply.github.com>
2020-01-04 16:31:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,10 +16,6 @@
       <li class="nav-item">
         <a class="nav-link" href="https://experime.azurewebsites.net/analyze.php">Add Image</a>
       </li>
-  <li class="nav-item">
-        <a class="nav-link" href="https://experime.azurewebsites.net/computervision.php">Analyze image</a>
-      </li>
-    </ul>
   </div>
 </nav>
 	 
